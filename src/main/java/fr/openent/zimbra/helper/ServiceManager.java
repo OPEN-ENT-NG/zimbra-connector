50dfb8f6c4394d77f5241a23d3ef367957821f65
Thomas Coleno <thomas.coleno@cgi.com>
2018-11-15 11:06:50 +0100

    MailAddress and serviceManager refactoring

---
+++ src/main/java/fr/openent/zimbra/helper/ServiceManager.java
@@ -0,0 +1,100 @@
+package fr.openent.zimbra.helper;
+
+import fr.openent.zimbra.service.impl.*;
+import fr.openent.zimbra.service.synchro.SynchroUserService;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonObject;
+import org.entcore.common.notification.TimelineHelper;
+
+public class ServiceManager {
+
+    private static ServiceManager serviceManager = null;
+
+    private TimelineHelper timelineHelper;
+    private UserService userService;
+    private FolderService folderService;
+    private AttachmentService attachmentService;
+    private MessageService messageService;
+    private SignatureService signatureService;
+    private SqlZimbraService sqlService;
+    private NotificationService notificationService;
+    private CommunicationService communicationService;
+    private GroupService groupService;
+
+
+    private void initServiceManager(Vertx vertx, JsonObject config, EventBus eb, String pathPrefix) {
+
+        timelineHelper = new TimelineHelper(vertx, eb, config);
+        this.sqlService = new SqlZimbraService(vertx, config.getString("db-schema", "zimbra"));
+        SoapZimbraService soapService = new SoapZimbraService(vertx, config);
+        SynchroUserService synchroUserService = new SynchroUserService(soapService, sqlService);
+        this.userService = new UserService(soapService, synchroUserService, sqlService);
+        this.folderService = new FolderService(soapService);
+        this.signatureService = new SignatureService(userService, soapService);
+        this.messageService = new MessageService(soapService, folderService,
+                sqlService, userService, synchroUserService);
+        this.attachmentService = new AttachmentService(soapService, messageService, vertx, config);
+        this.notificationService = new NotificationService(soapService, userService, pathPrefix, timelineHelper);
+        this.communicationService = new CommunicationService(messageService);
+        this.groupService = new GroupService(soapService, sqlService, synchroUserService);
+
+
+        soapService.setServices(userService, synchroUserService);
+        synchroUserService.setUserService(userService);
+    }
+
+    public static ServiceManager init(Vertx vertx, JsonObject config, EventBus eb, String pathPrefix) {
+        if(serviceManager == null) {
+            serviceManager = new ServiceManager();
+            serviceManager.initServiceManager(vertx, config, eb, pathPrefix);
+        }
+        return serviceManager;
+    }
+
+    public static ServiceManager getServiceManager() {
+        return serviceManager;
+    }
+
+    public TimelineHelper getTimelineHelper() {
+        return timelineHelper;
+    }
+
+    public SqlZimbraService getSqlService() {
+        return sqlService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public FolderService getFolderService() {
+        return folderService;
+    }
+
+    public SignatureService getSignatureService() {
+        return signatureService;
+    }
+
+    public MessageService getMessageService() {
+        return messageService;
+    }
+
+    public AttachmentService getAttachmentService() {
+        return attachmentService;
+    }
+
+    public NotificationService getNotificationService() {
+        return notificationService;
+    }
+
+    public CommunicationService getCommunicationService() {
+        return communicationService;
+    }
+
+    public GroupService getGroupService() {
+        return groupService;
+    }
+
+
+}
