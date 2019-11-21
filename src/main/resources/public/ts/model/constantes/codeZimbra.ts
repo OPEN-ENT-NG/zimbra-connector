c7836cc58765255cffac5717e27b7a5546d471d7
Simon LEDUNOIS <simon.ledunois@gmail.com>
2019-11-21 15:25:49 +0100

    clean code

--- src/main/resources/public/ts/model/constantes/codeZimbra.ts
+++ src/main/resources/public/ts/model/constantes/codeZimbra.ts
@@ -1,8 +1,7 @@
-export const CODEZIMBRA = {
-    SERVICE: {
-        INVALID_REQUEST:"service.INVALID_REQUEST",
-    },
-    MAIL: {
-        MESSAGE_TOO_BIG:"mail.MESSAGE_TOO_BIG"
-    }
+export enum SERVICE {
+    INVALID_REQUEST = "service.INVALID_REQUEST"
+}
+
+export enum MAIL {
+    MESSAGE_TOO_BIG = "mail.MESSAGE_TOO_BIG"
 }
