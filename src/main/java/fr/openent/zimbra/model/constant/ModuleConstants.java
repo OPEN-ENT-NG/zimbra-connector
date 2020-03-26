520f4ec2a360dbacf82baf105bf3e7eab2fe0ea6
Thomas Coleno <thomas.coleno@cgi.com>
2020-03-26 17:42:28 +0100

    Add RedirectionService for write-mail

---
+++ src/main/java/fr/openent/zimbra/model/constant/ModuleConstants.java
@@ -0,0 +1,15 @@
+package fr.openent.zimbra.model.constant;
+
+public class ModuleConstants {
+
+    public static final String USERBOOK_ADDRESS = "userbook.preferences";
+
+    public static final String URL_PREAUTH = "preauth";
+    public static final String URL_ROOT = "zimbra";
+    public static final String URL_PREFIX = "/zimbra/";
+
+    public static final String URL_MIDDLE_WRITEMAIL = "#/write-mail/";
+
+    public static final String TYPE_GROUP = "Group";
+    public static final String TYPE_USER = "User";
+}
