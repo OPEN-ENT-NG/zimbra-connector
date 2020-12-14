22494242be02d00883cce206b6e863a73e5046a6
sjol <samuel.jollois@cgi.com>
2020-12-14 15:04:48 +0100

    copy json file in view folder when buildFront

--- gulpfile.js
+++ gulpfile.js
@@ -60,14 +60,14 @@ gulp.task('rev', ['webpack'], function () {
 gulp.task('build', ['rev'], function () {
     var streams = [];
     apps.forEach(function (app) {
-        streams.push(gulp.src("./" + app + "/src/main/resources/view-src/**/*.html")
+        streams.push(gulp.src("./" + app + "/src/main/resources/view-src/**/*.+(html|txt|json)")
             .pipe(revReplace({manifest: gulp.src("./" + app + "/rev-manifest.json")}))
             .pipe(gulp.dest("./" + app + "/src/main/resources/view")));
         streams.push(gulp.src("./" + app + "/src/main/resources/public/dist/behaviours.js")
             .pipe(gulp.dest("./" + app + "/src/main/resources/public/js")));
     });
     apps_viewonly.forEach(function (app) {
-        streams.push(gulp.src("./" + app + "/src/main/resources/view-src/**/*.html")
+        streams.push(gulp.src("./" + app + "/src/main/resources/view-src/**/*.+(html|txt|json)")
                     .pipe(revReplace({manifest: gulp.src("./" + app + "/rev-manifest.json")}))
                     .pipe(gulp.dest("./" + app + "/src/main/resources/view")));
     });
