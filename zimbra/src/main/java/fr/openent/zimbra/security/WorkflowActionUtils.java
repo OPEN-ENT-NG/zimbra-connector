81bf4b83d9a6b21e1f00657c4c30471b330b0775
alicedraillard <alice.draillard@cgi.com>
2023-01-09 11:48:51 +0100

    feat(calendar) : #MZI-108 check if user has zimbra expert

--- zimbra/src/main/java/fr/openent/zimbra/security/WorkflowActionUtils.java
+++ zimbra/src/main/java/fr/openent/zimbra/security/WorkflowActionUtils.java
@@ -21,13 +21,13 @@ import org.entcore.common.user.UserInfos;
 
 import java.util.List;
 
-final class WorkflowActionUtils {
+public final class WorkflowActionUtils {
 
     private WorkflowActionUtils() {
         throw new IllegalAccessError("Utility class");
     }
 
-    static boolean hasRight (UserInfos user, String action) {
+    public static boolean hasRight(UserInfos user, String action) {
         List<UserInfos.Action> actions = user.getAuthorizedActions();
         for (UserInfos.Action userAction : actions) {
             if (action.equals(userAction.getDisplayName())) {
