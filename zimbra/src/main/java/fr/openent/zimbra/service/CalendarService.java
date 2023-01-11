9964c4701de74981080259385184f4d6e428e01a
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2023-01-11 15:10:55 +0100

    feat(calendar) : #MZI-105 get ical from zimbra api (#44)

---
+++ zimbra/src/main/java/fr/openent/zimbra/service/CalendarService.java
@@ -0,0 +1,23 @@
+package fr.openent.zimbra.service;
+
+import io.vertx.core.Future;
+import org.entcore.common.user.UserInfos;
+
+public interface CalendarService {
+    /**
+     * Gets ICal text from Zimbra API
+     * @param user {@link UserInfos} the user
+     * @return {@link String} the ICal text
+     */
+    Future<Void> getICal(UserInfos user);
+
+    /**
+     * Gets ICal text from Zimbra API
+     * @param user {@link UserInfos} the user
+     * @param rangeStart {@link Long} the range start in milliseconds to get the events
+     * @param rangeEnd {@link Long} the range end in milliseconds to get the events
+     * @return {@link String} the ICal text
+     */
+    Future<Void> getICal(UserInfos user, Long rangeStart, Long rangeEnd);
+
+}
---
+++ zimbra/src/main/java/fr/openent/zimbra/service/impl/CalendarServiceImpl.java
@@ -0,0 +1,60 @@
+package fr.openent.zimbra.service.impl;
+
+import fr.openent.zimbra.core.constants.Field;
+import fr.openent.zimbra.model.constant.SoapConstants;
+import fr.openent.zimbra.model.soap.model.SoapICalResponse;
+import fr.openent.zimbra.service.CalendarService;
+import fr.openent.zimbra.service.data.SoapZimbraService;
+import fr.wseduc.webutils.http.Renders;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.user.UserInfos;
+
+public class CalendarServiceImpl implements CalendarService {
+    protected static final Logger log = LoggerFactory.getLogger(Renders.class);
+
+    private final SoapZimbraService soapService;
+
+    public CalendarServiceImpl(SoapZimbraService soapService) {
+        this.soapService = soapService;
+    }
+
+    public Future<Void> getICal(UserInfos user) {
+        return getICal(user, null, null);
+    }
+
+    public Future<Void> getICal(UserInfos user, Long rangeStart, Long rangeEnd) {
+        Promise promise = Promise.promise();
+
+        JsonObject icalRequest = new JsonObject()
+                .put(Field._JSNS, SoapConstants.NAMESPACE_MAIL);
+
+        if (rangeStart != null) {
+            icalRequest.put("s", rangeStart);
+        }
+
+        if (rangeEnd != null) {
+            icalRequest.put("e", rangeEnd);
+        }
+
+        JsonObject searchRequest = new JsonObject()
+                .put(Field.NAME, Field.GETICALREQUEST)
+                .put(Field.CONTENT, icalRequest);
+
+        soapService.callUserSoapAPI(searchRequest, user, getICalResponse -> {
+            if (getICalResponse.isLeft()) {
+                log.error(getICalResponse.left().getValue());
+                promise.fail("zimbra.error.ical.not.retrieved");
+            } else {
+                String ical = new SoapICalResponse(getICalResponse.right().getValue()).getContent();
+                promise.complete(ical);
+            }
+        });
+
+        return promise.future();
+    }
+
+}
