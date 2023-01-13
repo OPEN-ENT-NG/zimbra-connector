305dccf2ae03af0be6a4885b366d867522199ee9
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2023-01-13 16:44:11 +0100

    log: #MZI-141 display more info if code fails

--- zimbra/src/main/java/fr/openent/zimbra/service/data/SoapZimbraService.java
+++ zimbra/src/main/java/fr/openent/zimbra/service/data/SoapZimbraService.java
@@ -194,15 +194,16 @@ public class SoapZimbraService {
      * @return default handler
      */
     private Handler<HttpClientResponse> zimbraRequestHandler(final Future<JsonObject> handler) {
-        return response ->
-            response.bodyHandler( body -> {
+        return response -> {
+            response.bodyHandler(body -> {
                 JsonObject result;
                 try {
                     result = body.toJsonObject();
                 } catch (DecodeException e) {
-                    log.error("Can't process Zimbra response + " + response.statusMessage());
                     log.debug("Zimbra response details : " + body.toString());
                     handler.fail(response.statusMessage());
+                    String messageToFormat = "[Zimbra@%s::zimbraRequestHandler] An error occurred while processing zimbra response: %s, status: %s";
+                    log.error(String.format(messageToFormat, this.getClass().getSimpleName(), e.getMessage(), response.statusMessage()));
                     return;
                 }
                 if(response.statusCode() == 200) {
@@ -221,10 +222,18 @@ public class SoapZimbraService {
                                 .getString("Code"));
                         handler.complete(errorJson.put(IS_SUCCESSFUL, false));
                     } catch (Exception e) {
-                        handler.fail(response.statusMessage());
+                        String messageToFormat = "[Zimbra@%s::zimbraRequestHandler] An error has occurred during request: %s";
+                        log.error(String.format(messageToFormat, this.getClass().getSimpleName(), e));
+                        handler.fail(e.getMessage());
                     }
                 }
             });
+            response.exceptionHandler(err -> {
+                String messageToFormat = "[Zimbra@%s::zimbraRequestHandler] An error has occurred during request: %s";
+                log.error(String.format(messageToFormat, this.getClass().getSimpleName(), err));
+                handler.fail(err.getMessage());
+            });
+        };
     }
 
     /**
