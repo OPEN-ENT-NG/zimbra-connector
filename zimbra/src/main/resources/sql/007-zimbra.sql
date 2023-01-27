f4d20a3b004c490110c4a7d46cfb7e993ee21a9d
aymericdevriesere <aymeric.devriesere@cgi.com>
2023-01-27 16:38:15 +0100

    feat(recall): MZI-160

--- zimbra/src/main/resources/sql/007-zimbra.sql
+++ zimbra/src/main/resources/sql/007-zimbra.sql
@@ -3,6 +3,7 @@ CREATE TABLE zimbra.action(
     user_id bigint,
     date timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
     type varchar,
+    approved boolean,
     CONSTRAINT action_pkey PRIMARY KEY (id)
 );
 CREATE TABLE zimbra.task(
@@ -10,6 +11,19 @@ CREATE TABLE zimbra.task(
     action_id integer,
     status varchar,
     CONSTRAINT task_pkey PRIMARY KEY (id),
+    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.action (id)
+);
+CREATE TABLE zimbra.recalled_mail
+(
+    id serial NOT NULL,
+    action_id integer,
+    user_name character varying,
+    user_mail character varying,
+    structures json,
+    object character varying,
+    comment text,
+    mail_date character varying,
+    PRIMARY KEY (id),
     CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.action (id)
 );
 CREATE TABLE zimbra.task_logs(
@@ -26,21 +40,4 @@ CREATE TABLE zimbra.recall_recipient
     retry int,
     CONSTRAINT mail_id_fkey FOREIGN KEY (mail_id) REFERENCES zimbra.recalled_mail (id)
 ) INHERITS (zimbra.task);
-CREATE TABLE zimbra.recalled_mail
-(
-    id serial NOT NULL,
-    user_id character varying(36),
-    action_id integer,
-    user_name character varying,
-    user_mail character varying,
-    structures json,
-    mail_id character varying,
-    object character varying,
-    number_message bigint,
-    comment text,
-    recipient jsonb,
-    mail_date character varying,
-    PRIMARY KEY (mail_id, user_id),
-    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.action (id)
-);
 
