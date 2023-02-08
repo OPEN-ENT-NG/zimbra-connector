2474b0c4344fad1c827900cf709e5e9fabcbeaa6
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2023-02-08 16:59:57 +0100

    feat(action): MZI-166 add models and services (#70)

---
+++ zimbra/src/main/java/fr/openent/zimbra/model/action/Action.java
@@ -0,0 +1,104 @@
+package fr.openent.zimbra.model.action;
+
+import fr.openent.zimbra.core.constants.Field;
+import fr.openent.zimbra.core.enums.ActionType;
+import fr.openent.zimbra.model.task.Task;
+import io.vertx.core.json.JsonObject;
+import java.time.Instant;
+import java.util.*;
+
+public class Action {
+    protected long id;
+    protected UUID userId;
+    protected Date createdAt;
+    protected ActionType actionType;
+    protected boolean approved;
+
+    protected Set<Task> tasks;
+
+    public Action(UUID userId, ActionType actionType, boolean approved) {
+        this.userId = userId;
+        this.actionType = actionType;
+        this.approved = approved;
+    }
+
+    public Action(long id, UUID userId, Date createdAt, ActionType actionType, boolean approved) {
+        this.id = id;
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.actionType = actionType;
+        this.approved = approved;
+        tasks = new HashSet<>();
+    }
+    private static boolean JSONContainsActionData (JsonObject actionData) {
+        return  actionData.containsKey(Field.ACTION_ID) &&
+                actionData.containsKey(Field.USER_ID) &&
+                actionData.containsKey(Field.CREATED_AT) &&
+                actionData.containsKey(Field.TYPE) &&
+                actionData.containsKey(Field.APPROVED);
+    }
+
+    public Action(JsonObject actionData) throws Exception {
+        if (!JSONContainsActionData(actionData)) {
+            throw new Exception(String.format("[Zimbra@%s::Action] Json does not match Action model", Action.class));
+        }
+        long id = actionData.getInteger(Field.ACTION_ID);
+        try {
+            UUID userId = UUID.fromString(actionData.getString(Field.USER_ID));
+            Date created_at = Date.from(Instant.parse(actionData.getString(Field.CREATED_AT)));
+            ActionType actionType = ActionType.fromString(actionData.getString(Field.TYPE));
+            boolean approved = actionData.getBoolean(Field.APPROVED);
+            this.id = id;
+            this.userId = userId;
+            this.createdAt = created_at;
+            this.actionType = actionType;
+            this.approved = approved;
+        } catch (Exception e) {
+            throw new Exception(String.format("[Zimbra@%s::Action] Bad field format", Action.class));
+        }
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public boolean getApproved() {
+        return approved;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void addTasks(List<Task> tasks) {
+        this.tasks.addAll(tasks);
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        this.tasks.remove(task);
+    }
+}
--- zimbra/src/main/java/fr/openent/zimbra/service/QueueService.java
+++ zimbra/src/main/java/fr/openent/zimbra/service/QueueService.java
@@ -1,43 +1,114 @@
 package fr.openent.zimbra.service;
 
+import fr.openent.zimbra.core.constants.Field;
+import fr.openent.zimbra.core.enums.ActionType;
+import fr.openent.zimbra.model.action.Action;
 import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-import org.entcore.common.user.UserInfos;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.sql.Sql;
+import org.entcore.common.sql.SqlResult;
 
-public interface QueueService {
-    /**
-     * Creates actions and tasks for request queue
-     * @param user the user {@link UserInfos}
-     * @param info the object with the needed info {@link JsonObject}
-     *             {
-     *                 type: String,
-     *                 data: JsonObject,
-     *                 approved: Boolean //default = false
-     *             }
-     * @return {@link Future<Void>}
-     */
-    Future<Void> putRequestInQueue(UserInfos user, JsonObject info);
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Abstract queue service
+ *
+ * @param <T> Task
+ * @param <V> TaskInfo
+ */
+public abstract class QueueService<T, V> {
+    protected static final Logger log = LoggerFactory.getLogger(QueueService.class);
+
+    protected final String schema;
+    protected final String actionTable;
+    protected String taskTable;
+    protected ActionType actionType;
+
+    public QueueService(String schema) {
+        this.schema = schema;
+        this.actionTable = schema + ".action";
+        this.taskTable = schema + ".task";
+    }
 
     /**
-     * Creates new action in related table for request queue
-     * @param user the user {@link UserInfos}
-     * @param type the request type {@link String}
-     * @param approved if the request has been approved (default = false) {@link Boolean}
-     * @return {@link Future<Integer>} the id of the created action
+     * Insert a list of Task in the worker queue
+     *
+     * @param tasks List of Task to insert
+     * @return
      */
-    Future<Integer> createActionInQueue(UserInfos user, String type, Boolean approved);
+    public abstract Future<List<T>> insertTasksInQueue(List<T> tasks);
 
     /**
-     * Creates ICal type task in related table for request queue
-     * @param actionId the id of the parent action {@link Integer}
-     * @param requestInfo the data needed {@link JsonObject}
-     *              {
-     *                 name: "GetICalRequest",
-     *                 content: {
-     *                      _jsns: "urn:zimbraMail"
-     *                 }
-     *               }
-     * @return {@link Future<Void>}
+     * Method to create Tasks
+     *
+     * @param action Action
+     * @param taskInfo Task info
+     * @return
      */
-    Future<Void> createICalTask(Integer actionId, JsonObject requestInfo);
+    protected abstract Future<List<T>> createTasks(Action action, V taskInfo);
+
+    /**
+     * Create an Action
+     *
+     * @param userId User id
+     * @param actionType Action type
+     * @param approved
+     * @return
+     */
+    protected Future<Action> createAction(UUID userId, ActionType actionType, boolean approved) {
+        Promise<Action> promise = Promise.promise();
+
+        StringBuilder query = new StringBuilder();
+
+        query.append("INSERT INTO ")
+                .append(actionTable)
+                .append(" (" + Field.USER_ID + "," + Field.TYPE + "," + Field.APPROVED + ") ")
+                .append("VALUES (?, ?, ?)")
+                .append("RETURNING " + Field.ACTION_ID + "," + Field.USER_ID + "," + Field.CREATED_AT + "," + Field.TYPE + "," + Field.APPROVED);
+
+        JsonArray values = new JsonArray();
+        values.add(userId.toString()).add(actionType.method()).add(approved);
+
+        Sql.getInstance().prepared(query.toString(), values, SqlResult.validUniqueResultHandler(handler -> {
+            if (handler.isRight()) {
+                long id = handler.right().getValue().getLong(Field.ID);
+                Date createdAt = Date.from(Instant.parse(handler.right().getValue().getString(Field.CREATED_AT)));
+                Action action = new Action(id, userId, createdAt, actionType, approved);
+                promise.complete(action);
+            } else {
+                String errMessage = String.format("[Zimbra@%s::createAction]:  " +
+                                "an error has occurred while creating action in queue: %s",
+                        this.getClass().getSimpleName(), handler.left().getValue());
+                log.error(errMessage);
+                promise.fail("zimbra.error.queue.action");
+            }
+        }));
+
+        return promise.future();
+    }
+
+    /**
+     * Create and insert tasks in the worker queue
+     *
+     * @param userId User id
+     * @param taskInfo Task data
+     * @return
+     */
+    public Future<List<T>> createAndInsertTasksInQueue(UUID userId, V taskInfo) {
+        Promise<List<T>> promise = Promise.promise();
+
+        this.createAction(userId, this.actionType, false)
+                .compose(action -> this.createTasks(action, taskInfo))
+                .compose(this::insertTasksInQueue)
+                .onSuccess(promise::complete)
+                .onFailure(promise::fail);
+
+        return promise.future();
+    }
 }
