b67f20c795740166e818489b26c914f26c1b39fe
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2023-05-04 17:16:16 +0200

    feat(recall): #MZI-214 update API data for entcore front

--- zimbra/src/main/java/fr/openent/zimbra/utils/DateUtils.java
+++ zimbra/src/main/java/fr/openent/zimbra/utils/DateUtils.java
@@ -11,7 +11,7 @@ public final class DateUtils {
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATE_FORMAT_UTC = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String DATE_FORMAT_SQL = "yyyy-MM-dd'T'HH:mm:ss.SSS";
-    public static final String DATE_FORMAT_SQL_WHITHOUT_MILLI = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String DATE_FORMAT_SQL_WITHOUT_MILLI = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String FRENCH_DATE_FORMAT ="dd-MM-yyyy";
     public static final String YEAR = "yyyy";
     public static final String DAY_MONTH_YEAR_HOUR_TIME = "dd/MM/yyyy HH:mm:ss";
