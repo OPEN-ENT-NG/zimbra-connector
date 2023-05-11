957bb3fee81515d9f36dbfe350123630ce8a3ffc
aymericdevriesere <101819152+aymericdevriesere@users.noreply.github.com>
2023-05-11 13:33:03 +0200

    fix(recall): #MZI-214 update table creation script

--- zimbra/src/main/resources/sql/010-zimbra-action.sql
+++ zimbra/src/main/resources/sql/010-zimbra-action.sql
@@ -13,7 +13,7 @@ CREATE TABLE zimbra.tasks
     action_id bigint,
     status    varchar,
     CONSTRAINT task_pkey PRIMARY KEY (id),
-    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.actions (id) ON DELETE CASCADE
+    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.actions (id)
 );
 CREATE TABLE zimbra.recall_mails
 (
@@ -26,40 +26,26 @@ CREATE TABLE zimbra.recall_mails
     object     varchar,
     comment    text,
     mail_date  timestamp with time zone,
-    completed boolean,
-    local_mail_id varchar,
     CONSTRAINT recalled_mail_pkey PRIMARY KEY (id),
-    CONSTRAINT unique_local_id UNIQUE(local_mail_id),
-    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.actions (id) ON DELETE CASCADE
+    CONSTRAINT action_id_fkey FOREIGN KEY (action_id) REFERENCES zimbra.actions (id)
+);
+CREATE TABLE zimbra.task_logs
+(
+    id      bigserial NOT NULL,
+    task_id bigint,
+    logs    varchar,
+    CONSTRAINT task_logs_pkey PRIMARY KEY (id),
+    CONSTRAINT task_id_fkey FOREIGN KEY (task_id) REFERENCES zimbra.tasks (id)
 );
 CREATE TABLE zimbra.recall_recipient_tasks
 (
     recall_mail_id bigint,
     receiver_id    uuid,
     retry          smallint,
-    recipient_address varchar,
-    CONSTRAINT mail_id_fkey FOREIGN KEY (recall_mail_id) REFERENCES zimbra.recall_mails (id) ON DELETE CASCADE,
-    CONSTRAINT unique_recall_task_id UNIQUE(id)
+    CONSTRAINT mail_id_fkey FOREIGN KEY (recall_mail_id) REFERENCES zimbra.recall_mails (id)
 ) INHERITS (zimbra.tasks);
 CREATE TABLE zimbra.ical_request_tasks
 (
     name varchar(64),
-    body varchar,
-    CONSTRAINT unique_ical_task_id UNIQUE(id)
+    body varchar
 ) INHERITS (zimbra.tasks);
-CREATE TABLE zimbra.recall_task_logs
-(
-    id      bigserial NOT NULL,
-    recall_task_id bigint,
-    logs    varchar,
-    CONSTRAINT recall_task_logs_pkey PRIMARY KEY (id),
-    CONSTRAINT recall_task_id_fkey FOREIGN KEY (recall_task_id) REFERENCES zimbra.recall_recipient_tasks(id) ON DELETE CASCADE
-);
-CREATE TABLE zimbra.ical_task_logs
-(
-    id      bigserial NOT NULL,
-    ical_task_id bigint,
-    logs    varchar,
-    CONSTRAINT ical_task_logs_pkey PRIMARY KEY (id),
-    CONSTRAINT ical_task_id_fkey FOREIGN KEY (ical_task_id) REFERENCES zimbra.ical_request_tasks(id) ON DELETE CASCADE
-);
