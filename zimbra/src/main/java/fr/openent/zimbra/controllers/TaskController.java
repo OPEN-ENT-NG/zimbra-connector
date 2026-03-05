40b911246009459b3c783b49a315ecbc3c05816e
mariusestaque <marius.estaque@opendigitaleducation.com>
2026-03-05 18:20:41 +0100

    chore: exposing cron tasks to be triggered by api call

---
+++ zimbra/src/main/java/fr/openent/zimbra/controllers/TaskController.java
@@ -0,0 +1,55 @@
+package fr.openent.zimbra.controllers;
+
+import fr.openent.zimbra.service.synchro.SynchroTask;
+import fr.openent.zimbra.tasks.cron.ICalRequestCron;
+import fr.openent.zimbra.tasks.cron.RecallMailCron;
+import fr.wseduc.rs.Post;
+import fr.wseduc.webutils.http.BaseController;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class TaskController extends BaseController {
+	protected static final Logger log = LoggerFactory.getLogger(TaskController.class);
+
+	final SynchroTask syncLauncherTask;
+	final RecallMailCron recallMailCron;
+	final ICalRequestCron iCalRequestCron;
+	final SynchroTask syncMailerTask;
+
+	public TaskController(SynchroTask syncLauncherTask, RecallMailCron recallMailCron, ICalRequestCron iCalRequestCron, SynchroTask syncMailerTask) {
+		this.syncLauncherTask = syncLauncherTask;
+		this.recallMailCron = recallMailCron;
+		this.iCalRequestCron = iCalRequestCron;
+		this.syncMailerTask = syncMailerTask;
+	}
+
+	@Post("api/internal/sync-launcher")
+	public void syncLauncher(HttpServerRequest request) {
+		log.info("Trigger sync launcher task");
+		syncLauncherTask.handle(0L);
+		render(request, null, 202);
+	}
+
+	@Post("api/internal/recall-mail")
+	public void recallMail(HttpServerRequest request) {
+		log.info("Trigger recall mail task");
+		recallMailCron.handle(0L);
+		render(request, null, 202);
+	}
+
+	@Post("api/internal/ical-request")
+	public void iCalRequest(HttpServerRequest request) {
+		log.info("Triggered ical request task");
+		iCalRequestCron.handle(0L);
+		render(request, null, 202);
+	}
+
+	@Post("api/internal/sync-mailer")
+	public void syncMailer(HttpServerRequest request) {
+		log.info("Triggered sync mailer task");
+		syncMailerTask.handle(0L);
+		render(request, null, 202);
+	}
+
+}
