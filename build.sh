ddb9aa1a0164ccdf65fac2343e550501712ac226
Junior Bernard <junior.bernard@opendigitaleducation.com>
2026-04-17 13:32:29 +0200

    ci: fix maven options

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ publish() {
       export nexusRepository='releases'
       ;;
   esac
-  docker compose run --rm maven mvn -DrepositoryId=ode-$nexusRepository -DskiptTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
+  docker compose run --rm maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -DskiptTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
 }
 
 publishNexus() {
@@ -95,7 +95,7 @@ publishNexus() {
     *SNAPSHOT) export nexusRepository='snapshots' ;;
     *)         export nexusRepository='releases' ;;
   esac
-  docker compose run --rm  maven mvn -DrepositoryId=ode-$nexusRepository -Durl=$repo -DskipTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
+  docker compose run --rm  maven mvn $MVN_OPTS -DrepositoryId=ode-$nexusRepository -Durl=$repo -DskipTests -Dmaven.test.skip=true --settings /var/maven/.m2/settings.xml deploy
 }
 
 # If DEBUG env var is set to "true" then set -x to enable debug mode
